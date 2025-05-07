9dd866864339d8711ee5c517124b31ad32ff1acb
Joshua Penn <joshualeepenn@gmail.com>
2025-05-07 17:46:25 -0400

    project creation

---
+++ src/test/java/com/joshualeepenn/healthtracker/HealthTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshualeepenn.healthtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
