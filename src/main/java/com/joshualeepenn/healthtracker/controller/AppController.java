e4e17685dd66c403b9316c881d2030a7b0ac51bb
Joshua Penn <joshualeepenn@gmail.com>
2025-05-12 09:23:19 -0400

    AppController - nest all endpoints under '/app' for consistent naming

--- src/main/java/com/joshualeepenn/healthtracker/controller/AppController.java
+++ src/main/java/com/joshualeepenn/healthtracker/controller/AppController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/app")
 public class AppController {
 
     private final AppStatus status;
